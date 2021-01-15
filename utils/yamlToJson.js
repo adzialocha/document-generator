03e550cfd09530b287c3fc1c5e3beddda860d9a3
Andreas Dzialocha <x12@adz.garden>
2021-01-15 16:24:56 +0100

    Update dependencies, fix breaking changes

--- utils/yamlToJson.js
+++ utils/yamlToJson.js
@@ -3,7 +3,7 @@ const yaml = require('js-yaml')
 module.exports = yamlToJson = str => {
   return new Promise((resolve, reject) => {
     try {
-      resolve(yaml.safeLoad(str, 'utf-8'))
+      resolve(yaml.load(str, 'utf-8'))
     } catch (err) {
       reject(err)
     }
